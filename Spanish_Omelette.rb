d64b47a9b6fe0dddf6d22c8c9b8c16edd5929b23
ChrisH81 <chrishurst1981@icloud.com>
2017-05-17 15:51:15 +0200

    minor adjustments

--- Spanish_Omelette.rb
+++ Spanish_Omelette.rb
@@ -21,6 +21,7 @@ ingredients = [
   { name: 'salt', quantity: 'A pinch of' },
 ]
 
+puts "\n"
 puts "*    Let's cook a Spanish Omelette!    *"
 print_divider
 
@@ -34,33 +35,89 @@ loop do
   puts "\n"
   print_divider
 
-  puts "Do you have all the ingredients for your Omelette? (Y/N): "
+  print "Do you have all the ingredients for your Omelette? (Y/N): "
   answer = gets.chomp.upcase
 
   if answer == 'N'
-    puts "Better go shopping before you start then."
+    puts "\n"
+    print "Better go shopping before you start then."
     print_progress_bar
   elsif answer == 'Y'
+    puts "\n"
     puts "Let's get started!"
     break
   else
+    puts "\n"
     puts "That's not a valid input. Please Try again."
     end
   end
 
   steps = [
     { description: "Scrape potatoes", action: "scrape_potatoes" },
-    { description: "Cut potatoes into thick slices", action: "cut_potatoes" },
-    { description: "Chop the onion", action: "generic_recipe_step" },
-    { description: "Heat the oil in a large frying pan", action: "generic_recipe_step" },
+    { description: "Cut potatoes into slices", action: "cut_potatoes" },
+    { description: "Chop onion", action: "generic_recipe_step" },
+    { description: "Heat the oil in a pan", action: "generic_recipe_step" },
     { description: "Add potatoes to the pan", action: "add_potatoes_to_pan" },
     { description: "Add onion to the pan", action: "generic_recipe_step" },
-    { description: "Strain potatoes and onions through a colander into a large bowl", action: "generic_recipe_step" },
+    { description: "Strain potatoes and onions", action: "generic_recipe_step" },
     { description: "Break the eggs", action: "break_eggs" },
-    { description: "Beat the eggs separately", action: "generic_recipe_step" },
-    { description: "Stir into the bowl the potatoes with plenty of salt", action: "generic_recipe_step" },
-    { description: "Heat a little of the strained oil in a smaller pan", action: "generic_recipe_step" },
-    { description: "Tip everything into the pan and cook on a moderate heat", action: "generic_recipe_step"},
-    { description: "When almost set, invert on a plate and slide back into the pan", action: "generic_recipe_step" },
-    { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
+    { description: "Beat the eggs", action: "generic_recipe_step" },
+    { description: "Stir into the potatoes into the bowl with the salt", action: "generic_recipe_step" },
+    { description: "Heat a little of the oil in a smaller pan", action: "generic_recipe_step" },
+    { description: "Put everything into the pan and cook on a moderate heat", action: "generic_recipe_step"},
+    { description: "When almost set, flip on to a plate and slide back into the pan", action: "generic_recipe_step" },
+    { description: "Cook for a few more minutes, then slide the omelette on to a plate and cool for a few minutes before serving with garnish", action: "generic_recipe_step" }
   ]
+
+  print_divider
+
+  puts "Here are the steps to follow:"
+
+  puts "\n"
+
+  steps.each_with_index do |step, index|
+    puts (index + 1).to_s + " - " + step[:description]
+  end
+
+  puts ""
+
+  print_divider
+
+  def generic_recipe_step
+  puts "On it!"
+  print_progress_bar
+end
+
+def scrape_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Scraping potato #{counter}"
+    print_progress_bar
+  end
+end
+
+def cut_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Cutting potato #{counter} into thick slices"
+    print_progress_bar
+  end
+end
+
+def add_potatoes_to_pan
+  for counter in 1..NUM_POTATOES
+    print "Adding potato #{counter} to the pan."
+    print_progress_bar
+  end
+end
+
+def break_eggs
+  counter = 0
+  while counter < NUM_EGGS
+    counter += 1
+    print "Breaking egg #{counter}"
+    print_progress_bar
+  end
+end
